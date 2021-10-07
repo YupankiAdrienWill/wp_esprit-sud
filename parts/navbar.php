032ca3c401591d7d14b3b508d6245b630e715f3c
WilliamsChazal <w.chazal@codeur.online>
2021-10-07 13:59:31 +0200

    nav encours

---
+++ parts/navbar.php
@@ -0,0 +1,27 @@
+<nav id="nav">
+<?php wp_nav_menu( array( 'theme_location' => 'main' ) ); ?>  
+      <div class="burger burger--logo">
+            <a href="index.php"><img src="../assets/ico/logo_esprit_sud_dessin_alpha.png" alt=""></a>
+      </div>
+      <div class="menu" id="navMenu">
+            <div class="forMobile">
+                  <a href="https://facebook.com"><img class="logoSocialNetwork" src="../assets/ico/facebook.png" alt=""></a>
+                  <div class="tel">06 21 00 45 10</div>
+            </div>
+            <div id="dropdown_catalog">
+                  <div class="dropdown_content" id="dropdown_catalog___content">
+
+                  </div>
+            </div>
+
+            <div id="dropdown_cmtp">
+                  <div class="dropdown_content" id="dropdown_cmtp___content">     
+                  </div>
+            </div>
+      </div>
+      <div class="burger" id="burger">
+            <div></div>
+            <div></div>
+            <div></div>
+      </div>
+</nav>
