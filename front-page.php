762c01f005f2eac2cb426bbc5ace00edd75531b9
WilliamsChazal <w.chazal@codeur.online>
2021-10-07 12:56:58 +0200

    second commit

--- front-page.php
+++ front-page.php
@@ -1,3 +1,25 @@
 <?php get_header();?>
+
+
 <h1>Esprit Sud</h1>
+
+<div class="main__container">
+    <section class='header'> 
+    i<?php if ( get_header_image() ) : ?>
+    <div id="site-header">
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+            <img src="<?php header_image(); ?>" width="<?php echo absint( get_custom_header()->width ); ?>" height="<?php echo absint( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
+        </a>
+    </div>
+<?php endif; ?>
+                </div>
+                <div class="carré">
+                    <span id='carré'>
+                        <p>Bienvenue dans l’univers aquatique et décoratif d’Esprit sud by CMTP <br></p>
+                        <p>Venez découvrir notre large gamme de produits et surprenez-vous à rêver de votre futur piscine à l’eau pure et crustalline arborée d’arbres exotiques et de sculptures</p>
+                    </span>
+                </div>
+            </div>
+     </section>
+</div>
 <?php get_footer();?>
--- functions.php
+++ functions.php
@@ -5,12 +5,12 @@
 add_theme_support( 'title-tag' );
 
 function register_assets(){
-	wp_enqueue_script(
+    wp_enqueue_script(
 		'script-burger',
-		get_template_directory_uri().'assets/scripts/burger.js',
+		get_template_directory_uri().'/assets/scripts/burger.js',
 		 array(),
 		'1.0');
-	wp_enqueue_script(
+/* 	wp_enqueue_script(
 		'script-contact',
 		get_template_directory_uri().'/assets/scripts/contact.js',
 		 array(),
@@ -34,7 +34,7 @@ function register_assets(){
 		'script-top',
 		get_template_directory_uri().'/assets/scripts/top.js',
 		 array(),
-		'1.0');
+		'1.0'); */
 
     wp_enqueue_style(
         'content-css',
@@ -57,5 +57,34 @@ function register_assets(){
         '1.0'
     );
 
+
 }
 add_action('wp_enqueue_scripts', 'register_assets');
+
+add_theme_support( 'custom-header' );
+function themename_custom_header_setup() {
+    $defaults = array(
+        // Default Header Image to display
+        'default-image'         => get_template_directory_uri() . '',
+        // Display the header text along with the image
+        'header-text'           => true,
+        // Header text color default
+        'default-text-color'        => '000',
+        // Header image width (in pixels)
+        'flex-width'    => true,
+        'width'             => 1080,
+        // Header image height (in pixels)
+        'flex-height'   => true,
+        'height'            => 198,
+        // Header image random rotation default
+        'random-default'        => false,
+        // Enable upload of image file in admin 
+        'uploads'       => false,
+        // function to be called in theme head section
+        'wp-head-callback'      => 'wphead_cb',
+        //  function to be called in preview page head section
+        'admin-head-callback'       => 'adminhead_cb',
+        // function to produce preview markup in the admin screen
+        'admin-preview-callback'    => 'adminpreview_cb',
+        );
+}
