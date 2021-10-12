c5331e76f7775d788f196ba5d9ea039d0d4765c9
WilliamsChazal <w.chazal@codeur.online>
2021-10-12 11:42:26 +0200

    front-page

--- front-page.php
+++ front-page.php
@@ -1,5 +1,35 @@
 <?php get_header();?>
 
-
-
+<div class="main__container--page">
+    <section class='header--page' style="background-image: url('<?php the_post_thumbnail_url() ?>');"> 
+            <div><h1><?php bloginfo('name' )?></h1></div>
+            <div class ='container__bandeau--page'>
+                <div><img src="../assets/ico/feuille-2.svg" alt="" srcset=""></div>
+                <div class='container__bandeau--2--page'>
+                    <div class="texte--page">
+                        <h1> Esprit Sud by CMTP,</h1>
+                        <h1><?php the_title(); ?></h1>
+                    </div>
+                    <div class="carré--page">
+                        <span id='carré--page'>
+                        <?php the_content(); ?>
+                        </span>
+                    </div>
+                </div>
+            </div>
+    </section>
+</div>
+<section class='page'>
+    <?php 
+    $page = get_page_by_title('Notre catalogue'); 
+    $content = apply_filters('the_content', $page->post_content); 
+    $title = apply_filters('the_title', $page->post_title); 
+    $link = get_page_link( $page->permalink); 
+    $thumb = get_the_post_thumbnail( $page, 'post-thumbnail' );
+    ?>
+    <h2><?php echo $title; ?></h2>
+    <?php echo $content; ?>
+        <div> <?php echo $thumb; ?></div>
+        <div><a href="<?php echo $link; ?>"><button>Venez découvir nos produits</button></a></div>
+</section >
 <?php get_footer();?>
--- templates/catalogue.php
+++ templates/catalogue.php
@@ -13,11 +13,12 @@
                 <div><img src="../assets/ico/feuille-2.svg" alt="" srcset=""></div>
                 <div class='container__bandeau--2--page'>
                     <div class="texte--page">
+                        <h1> Esprit Sud by CMTP,</h1>
                         <h1><?php the_title(); ?></h1>
                     </div>
                     <div class="carré--page">
                         <span id='carré--page'>
-                        <H1><?php the_excerpt(); ?></H1>
+                        <H2><?php the_excerpt(); ?></H2>
                         </span>
                     </div>
                 </div>
