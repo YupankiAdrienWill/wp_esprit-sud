8622ded38b0cb4252a32d17d582c55a4de4dc197
WilliamsChazal <w.chazal@codeur.online>
2021-10-12 10:36:15 +0200

    catalogue

--- single-catalogue.php
+++ single-catalogue.php
@@ -1,7 +1,6 @@
 <?php get_header();?>
 <div class="main__container--produits">
-<section class='header--produits'> 
-<div class="produits-separateur"></div>
+<section class='header--produits'>
             <div class ='container__bandeau--produits'>
                 <div><img src="../assets/ico/feuille-2.svg" alt="" srcset=""></div>
                 <div class='container__bandeau--2--produits'>
