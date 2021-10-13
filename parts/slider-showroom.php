6b45cf97324d6859d199f526071e41d7a1352790
WilliamsChazal <w.chazal@codeur.online>
2021-10-13 12:58:49 +0200

    slider showroom ok

--- parts/slider-showroom.php
+++ parts/slider-showroom.php
@@ -1,15 +1,24 @@
 
 
 <section class="avis">
-      <?php $loop = new WP_Query((array('post_type' => 'showroom','order'=>'DSC', 'posts_per_page'=>1)));
-      while ( $loop->have_posts() ) : $loop->the_post(); ?>
-            <h1><?php the_title(); ?></h1>
-      <?php endwhile; wp_reset_query(); ?>
+<?php
+$nbr_avis=0;
+$loop = new WP_Query( array(
+    'post_type' => 'showroom'
+  )
+);
+
+?>
+            
+      
       <div class="parent_cadre_avis">
             <img class="return" id="avis_return" src="<?php bloginfo('template_directory'); ?>/assets/ico/circled-left.svg" alt="">
             <div class="cadre_avis">
+            <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+            <?php $nbr_avis++; ?>
                   <div class="cadre_avis__intermediaire" id="avisTranslate">
                         <div class="cadre_avis__child"><img   class='slideshow' src="<?= the_post_thumbnail_url(); ?>" style="width:100%"></div>
+                        <?php endwhile; wp_reset_query(); ?>
 <!--                         <div class="cadre_avis__child">avis 2</div>
                         <div class="cadre_avis__child">avis 3</div> -->
                   </div>
@@ -34,7 +43,7 @@
 
       let iAvis = 0;
       let width = 20; // definition width
-      let nbrAvisReel = 2; // definition nbr avis
+      let nbrAvisReel = <?=  $nbr_avis ?>; 
       let nbrAvis = nbrAvisReel - 1;
       document.getElementById('avis_return').onclick = function() {
             if (iAvis == 0) {
