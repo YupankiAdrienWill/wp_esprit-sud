c5e756d17183edc2dae5f3fead7dcebfbcb364f6
WilliamsChazal <w.chazal@codeur.online>
2021-10-13 10:40:54 +0200

    Slider Real

--- parts/slider-real.php
+++ parts/slider-real.php
@@ -3,7 +3,7 @@
 
   <!-- Full-width images with number text -->
       <div class="bigImg">
-            <?php $loop = new WP_Query((array('post_type' => 'realisation','order'=>'DSC', 'posts_per_page'=>1)));
+            <?php $loop = new WP_Query((array('post_type' => 'réalisation','order'=>'DSC', 'posts_per_page'=>1)));
             while ( $loop->have_posts() ) : $loop->the_post(); ?>
                   <div id="bigImg__child__" class="bigImg__child" style="background-image: url('<?= the_post_thumbnail_url(); ?>');"></div>
             <?php endwhile; wp_reset_query(); ?>
@@ -13,7 +13,7 @@
 
   <!-- Image text -->
   <div class="titleImg">
-  <?php $loop = new WP_Query((array('post_type' => 'realisation','order'=>'DSC', 'posts_per_page'=>1)));
+  <?php $loop = new WP_Query((array('post_type' => 'réalisation','order'=>'DSC', 'posts_per_page'=>1)));
    while ( $loop->have_posts() ) : $loop->the_post(); ?>
       <div id='title'> <?php the_title();?></div>
     <?php endwhile; wp_reset_query(); ?>
@@ -22,7 +22,7 @@
   <!-- Thumbnail images -->
 
   <div class="gallerie">
-      <?php $loop = new WP_Query((array('post_type' => 'realisation','order'=>'DSC', 'posts_per_page'=>50)));
+      <?php $loop = new WP_Query((array('post_type' => 'réalisation','order'=>'DSC', 'posts_per_page'=>50)));
       while ( $loop->have_posts() ) : $loop->the_post(); ?>
             <img src="<?= the_post_thumbnail_url(); ?>" alt="" onclick="changeback('<?= the_post_thumbnail_url(); ?>' , 'bigImg__child__','<?php the_title();?>')">
       <?php endwhile; wp_reset_query(); ?>
