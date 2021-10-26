f8bee301fd87f1309f208cfd7258a39719419a71
WilliamsChazal <w.chazal@codeur.online>
2021-10-26 11:36:59 +0200

    correction menu mobile

--- parts/navbar.php
+++ parts/navbar.php
@@ -23,17 +23,16 @@
       </div>
       <div class="menu" id="navMenu" >
             <div class="forMobile" id='forMobile'>
-            <div class='current_page'><p>Page actuelle :</p>
+          <!--   <div class='current_page'><p>Page actuelle :</p>
             <span><?php 
                   echo get_the_title( get_the_ID() );
                   ?></span>
-            </div>
-            
-                  <a href="https://facebook.com"><img class="logoSocialNetwork" src="<?php bloginfo('template_directory'); ?>/assets/ico/facebook.png" alt="logo facebook"></a>
-                  <div class="tel"><p>06 21 00 45 10</p></div>
-                  <span id="close" onclick="closeFunction()">&times;</span> 
+            </div> -->
+            <div class="tel"><p>06 21 00 45 10</p></div>
+            <a href="https://www.facebook.com/Esprit-Sud-by-CMTP-351693899243668/" target='_blank'><img class="logoSocialNetwork" src="<?php bloginfo('template_directory'); ?>/assets/ico/facebook.png" alt="logo facebook"></a>
+  
                  <div> <?php wp_nav_menu( array( 'theme_location' => 'main' ) ); ?></div>
-                 
+                 <span id="close" onclick="closeFunction()">&times;</span> 
                    
             </div>
               
--- parts/navbarCmtp.php
+++ parts/navbarCmtp.php
@@ -8,7 +8,7 @@
             <img class="logoTxt" src="<?php {echo bloginfo('template_directory').'/assets/ico/logo_cmtp.png';} ?>" alt="logo esprit sud">
       </div>
       <div class="bandeau__child">
-            <a href="https://facebook.com"><img class="logoSocialNetwork" src="<?php bloginfo('template_directory'); ?>/assets/ico/facebook.png" alt="logo facebook"></a>
+             <a href="https://www.facebook.com/Esprit-Sud-by-CMTP-351693899243668/" target='_blank'><img class="logoSocialNetwork" src="<?php bloginfo('template_directory'); ?>/assets/ico/facebook.png" alt="logo facebook"></a>
             <div class="tel">06 21 00 45 10</div>
       </div>
 </header>
@@ -21,17 +21,17 @@
       </div>
       <div class="menu" id="navMenu" >
             <div class="forMobile" id='forMobile'>
-            <div class='current_page'><p>Page actuelle :</p>
+          <!--   <div class='current_page'><p>Page actuelle :</p>
             <span><?php 
                   echo get_the_title( get_the_ID() );
                   ?></span>
-            </div>
-            
-            <a href="https://www.facebook.com/Esprit-Sud-by-CMTP-351693899243668/" target='_blank'>
-                  <div class="tel"><p>06 21 00 45 10</p></div>
-                  <span id="close" onclick="closeFunction()">&times;</span> 
+            </div> -->
+            <div class="tel"><p>06 21 00 45 10</p></div>
+            <a href="https://www.facebook.com/Esprit-Sud-by-CMTP-351693899243668/" target='_blank'><img class="logoSocialNetwork" src="<?php bloginfo('template_directory'); ?>/assets/ico/facebook.png" alt="logo facebook"></a>
+                  
+                  
                  <div> <?php wp_nav_menu( array( 'theme_location' => 'main' ) ); ?></div>
-                 
+                 <span id="close" onclick="closeFunction()">&times;</span> 
                     
             </div>
               
